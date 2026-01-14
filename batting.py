b2e0e7351a738d952ead89f46ac980252c5df9f3
Mohammed Ali <maazali1406@gmail.com>
2026-01-14 19:53:01 +0530

    Improve exception handling in user_bat function
    Refactor user_bat function to handle exceptions more specifically and improve shot selection logic.

--- batting.py
+++ batting.py
@@ -9,16 +9,21 @@ SHOT_NAMES = {
 
 def user_bat():
     try:
-        print(SHOT_NAMES)
-        shot = int(input("Select shot (1-4): "))
-        if shot not in SHOT_NAMES:
-            raise ValueError
-        print(f"You played {SHOT_NAMES[shot]}")
-        return shot
-    except:
-        print("❌ Invalid shot. Try again.")
-        return user_bat()
-
+        try:
+            print(SHOT_NAMES)
+            shot = int(input("Select shot (1-4): "))
+            if shot not in SHOT_NAMES:
+                raise ValueError
+            else:
+                print(f"You played {SHOT_NAMES[shot]}")
+                return shot
+        except (ValueError, NameError):
+            print("❌ Invalid shot. Try again.")
+            return user_bat()
+    except (EOFError , KeyboardInterrupt):
+        print("\nBye Bye !!")
+        print("Computer wins the game")
+        return exit()
 
 def computer_bat():
     return random.choice([1, 1, 2, 3, 3, 4, 4])
